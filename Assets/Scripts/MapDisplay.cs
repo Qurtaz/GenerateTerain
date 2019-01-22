ecceff6dacb6d8806f77507d1bca606cff278fa5
Qurtaz <polokolilol@o2.pl>
2019-01-22 15:08:11 +0100

    add generate Noise, and Edit Unity UI

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MapGenerator))]
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRenderer;
+
+    public void DrawMap(float [,] noiseMap)
+    {
+        int widht = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Texture2D texture = new Texture2D(widht, height);
+
+        Color[] colourMap = new Color[widht*height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < widht; x++)
+            {
+                colourMap[y * widht + x] = Color.Lerp(Color.black, Color.white, noiseMap[x,y]);
+            }
+        }
+        texture.SetPixels(colourMap);
+        texture.Apply();
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(widht, 1, height);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Noise
+{
+    public static float[,] GenerateNoiseMape(int mapWidht,int mapHeight, float sacle)
+    {
+        float[,] noiseMap = new float[mapWidht, mapHeight];
+        if(sacle <=0)
+        {
+            sacle = 0.00001f;
+        }
+        for(int y = 0; y <mapHeight; y++)
+        {
+            for(int x =0; x< mapWidht; x++)
+            {
+                float sampleX = x/sacle;
+                float sampleY = y/sacle;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX,sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
